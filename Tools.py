447b6ea72e3c12a40786ff8a9ee620fc2939ba50
https://github.com/Fajar-24/DARKFB <65275087+Fajar-24@users.noreply.github.com>
2020-05-17 14:49:10 +0700

    Update and rename Fb to Tools.py

---
+++ Tools.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python2
+# coding=utf-8
+
+clear
+echo         "Welcome"
+echo "==========================>
+echo "Author : FajarCyber"
+echo "Kontak : 08817805411"
+echo "Github : https://github.co>
+echo "==========================>
+echo  "Maaf Tools Tersedia Hanya>
+echo  "Untuk Username Dan Passwo>
+echo "pilih menu Tools nya"
+echo
+echo "[1] DarkFB"
+echo
+read -p "[?] pilih :" pil:
+# Batas gan
+clear
+if [ $pil -1 ]
+then
+ apt upgrade && apt update
+ apt install urllib
+ apt install python2
+ apt install requests
+ apt install mechanize
+ apt install git
+ git clone https://github.com/Fa>
+ cd CyberNEW
+python2 Cyber.py
+fi
