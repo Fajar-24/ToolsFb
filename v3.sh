7fd3dc5cd2c196e387068105d92a3820929d6ae9
https://github.com/Fajar-24/DARKFB <65275087+Fajar-24@users.noreply.github.com>
2020-06-03 04:02:51 +0700

    Update v3.sh

--- v3.sh
+++ v3.sh
@@ -2,24 +2,24 @@
 # coding=utf-8
 
 clear
-figlet "Welcome"lolcat
+figlet "Welcome"
 echo "•×•×•×•×•×•×•×•×•×•×•×•×•×•÷•×•÷•÷•÷•×•×•"
 echo "Author : FajarCyber"
-echo "••••••••••••••••••••••"
 echo "Kontak : 08817805411"
-echo "••••••••••••••••••••••"
 echo "Github : https://github.com/Fajar-24"
 echo "••••••••••••••••••••••••••••••••••••••"
-echo  " JANGAN DI SALAH GUNAKAN "
+echo  " JANGAN DI SALAH GUNAKAN TOD "
 figlet "EROR"
 echo "•×•×•×•×•×•×•×•×•×•×•×•×•×•×•×•×•×•×•×•××"
 echo "Maaf Tools Tersedia Hanya 1"
 echo "Untuk Username Dan Password Hubungi 08817805411"
 echo "GUNAKAN TOOLS INI DENGAN BIJAK"
 echo "pilih menu Tools nya"
-#
+echo
+echo
 echo "1.[÷] DARK FB"
 echo
+echo 
 read -p "[?] pilih :" pil:
 # Batas gan
 clear
